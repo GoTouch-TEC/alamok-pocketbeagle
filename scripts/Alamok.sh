7003bee6d7a46c65ce731e632af3cf123e941efb
Gustavo Segura <yo@gustavosegura.com>
2018-09-25 09:56:08 -0600

    [fix]	Split the setup for the diferent subsystems [fix]	Configure pins and inicialize gpsd on the same script

--- scripts/Alamok.sh
+++ scripts/Alamok.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-/bin/sleep 10
+/usr/bin/killall gpsd
+/usr/bin/config-pin P2.09 uart
+/usr/bin/config-pin P2.11 uart
+/usr/bin/config-pin P2.07 uart
+/usr/bin/config-pin P2.05 uart
+/usr/sbin/gpsd /dev/ttyO1 -F /var/run/gpsd.sock
+/usr/bin/pon fona
+/bin/sleep 2
 python3 -O /home/debian/alamok/Alamok.py -c /home/debian/mqtt_defaults.json -o /home/debian/Alamok.db
