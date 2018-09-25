65fc79756b6812943a86b9c8dde0be30cf1744ea
Gustavo Segura <yo@gustavosegura.com>
2018-09-25 10:01:33 -0600

    [fix]	kill programs using the serial port [fix]	Configure pins on the same script [fix]	Inicialize the ShutdownSignal service after the PowerManager

--- scripts/PowerManager.sh
+++ scripts/PowerManager.sh
@@ -1,4 +1,13 @@
 #! /bin/bash
+#! /bin/bash
+config-pin P1.08 uart
+config-pin P1.10 uart
+config-pin p1.33 gpio
+config-pin p1.34 gpio
+config-pin p1.36 gpio
+config-pin p1.35 in
+config-pin  p2.18 hi
+config-pin  p2.6 hi
+config-pin  p2.4 in
 INSTALL_PATH="/opt/Alamok-Setup/scripts"
-
-python3 "${INSTALL_PATH}/PowerManager.py"
+python3 -O "${INSTALL_PATH}/PowerManager.py"
--- scripts/ShutdownSignal.sh
+++ scripts/ShutdownSignal.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 INSTALL_PATH="/opt/Alamok-Setup/scripts"
-
+lsof -t /dev/ttyO2 | xargs --no-run-if-empty kill -9
 /bin/systemctl list-jobs | egrep -q 'reboot.target.*start' || echo "shutdown"|python3 "${INSTALL_PATH}/ShutdownSignal.py"
 config-pin p1.33 lo
 config-pin p1.34 lo
